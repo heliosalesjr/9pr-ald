5943fc3112fe1529e6891776404b85f6ae3d7177
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-05-20 19:59:30 -0300

    seed logo added

--- app/components/TwoColumnLayout.jsx
+++ app/components/TwoColumnLayout.jsx
@@ -4,7 +4,7 @@ import ResourceCards from './ResourcesCards';
 
 const TwoColumnLayout = () => {
   return (
-    <div className="relative h-[80vh] flex items-center justify-center bg-gradient-to-br from-yellow-700 to-green-300 z-0">
+    <div className="relative h-[95vh] flex items-center justify-center bg-gradient-to-br from-yellow-700 to-green-300 z-0">
       {/* Imagem de fundo e gradiente */}
      
      
@@ -19,6 +19,20 @@ const TwoColumnLayout = () => {
           src="/logo-white.png"
           alt="Logo BEĨ"
           width={70}
+          height={70}
+          className="mx-8"
+        />
+      </a>
+
+      <a
+        href="/"
+       
+        className="absolute top-4 right-4 z-10"
+      >
+        <Image
+          src="/seed_h.png"
+          alt="Logo SEED"
+          width={150}
           height={70}
           className="mx-8"
         />
